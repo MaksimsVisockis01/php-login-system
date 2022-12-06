cfd13a738283f0c6a46e091d022bb1fcbaec350d
[Students] Maksims Visockis <01DPMVisockis@rvt.lv>
2022-12-06 11:03:52 +0200

    signin system

--- php-login/includes/signup.inc.php
+++ php-login/includes/signup.inc.php
@@ -1,8 +1,40 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    echo "Signed up";
-}
-else{
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbh.inc.php';
+    require_once 'funcions.inc.php';
+
+    if(emtyImputSignup($name, $email, $username, $pwd, $pwdrepeat) !== false) {
+        header("location: ../signup.php?error=emptyinput");
+        exit();
+    }
+    if(invalidUid($username) !== false) {
+        header("location: ../signup.php?error=invaliduid");
+        exit();
+    }
+    if(invalidEmail($email) !== false) {
+        header("location: ../signup.php?error=invalidemail");
+        exit();
+    }
+    if(pwdMatch($pwd, $pwdrepeat) !== false) {
+        header("location: ../signup.php?error=passwordsdontmatch");
+        exit();
+    }
+    if(uidExists($conn, $username, $email) !== false) {
+        header("location: ../signup.php?error=usernametaken");
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $pwd);
+
+}else{
     header("location: ../signup.php");
+    exit();
 }
